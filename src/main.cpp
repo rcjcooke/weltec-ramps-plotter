14ff21ad45025c5c9d60d0bdba118fc033666065
Ray Cooke <rcjcooke@gmail.com>
2018-05-12 16:18:34 +1200

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,10 @@
 Plotter* mPlotter;
 MainMenu* mMainMenu;
 
-void testCode() {
+/*******************************
+ * Physical Test Methods
+ *******************************/
+void testCalibration() {
   delay(10000);
   Serial.println("Calibrate X");
   mPlotter->calibrate(Axis::X);
@@ -18,21 +21,30 @@ void testCode() {
   mPlotter->calibrate(Axis::Z);
 }
 
+void triangleTest() {
+  Point* p1 = new Point(30,30);
+  Point* p2 = new Point(50,70);
+  Point* p3 = new Point(46,90);
+  mPlotter->drawTriangle(p1, p2, p3);
+}
+
+/*******************************
+ * Setup Methods
+ *******************************/
+
 void setup() {
   // Serial interface setup
   Serial.begin(115200);
 
   // Creates Plotter controller and homes it
   mPlotter = new Plotter();
-  Point* p1 = new Point(30,30);
-  Point* p2 = new Point(50,70);
-  Point* p3 = new Point(46,90);
-  mPlotter->drawTriangle(p1, p2, p3);
-  //mPlotter->moveTo(new Point(20,20));
-  
-  // mMainMenu = new MainMenu(new SerialDisplayMenuConfiguration(SerialDisplayType::ansi_vt100, false, 1000), mPlotter);
+  mMainMenu = new MainMenu(new SerialDisplayMenuConfiguration(SerialDisplayType::ansi_vt100, false, 1000), mPlotter);
 }
 
+/*******************************
+ * Main Loop
+ *******************************/
+
 void loop() {
-  // SerialDisplayMenu::userInputLoop(mMainMenu);
+  SerialDisplayMenu::userInputLoop(mMainMenu);
 }
